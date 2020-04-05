4dcfcfaab406bf6379d931a97c2aa0da69630f0d
khaled-pc <khaledeltarabilyfciscu@gmail.com>
2020-04-05 23:01:14 +0200

    add typing feature for multi user

--- index.js
+++ index.js
@@ -8,17 +8,23 @@ app.use('/style', express.static(__dirname + '/style'))
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
 
 io.on('connection', (socket) => {
-  socket.username = 'anonymous';
+  socket.username = 'anonymous'+ Math.floor(Math.random() * 10) + 1  ;
   socket.on('change username', (name) => socket.username = name)
   socket.on('message', (msg) => io.emit('message',
   { 'user': socket.username, 'message': msg, 'datetime':moment().format('hh:mm')}))
   socket.on('join', (username) => {
     if (username != null) {
       socket.username = username
+    }else{
+        socket.emit('join',{'username':socket.username});
     }
     socket.broadcast.emit('message',
     { 'user': 'Server', 'message': socket.username + ' has joined!'})
   })
+  socket.on('typingMessage', (username) =>
+  socket.broadcast.emit("typingMessage", username ))
+  socket.on('stopTypingMessage', (username) =>
+  socket.broadcast.emit("stopTypingMessage",username))
 })
 
 http.listen(3000, () => console.log('listening on port 3000'))
